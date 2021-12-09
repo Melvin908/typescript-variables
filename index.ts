c9a18c22b98c5cfd01e66a4051b9b396fd5fc701
Melvin908 <melvincyriac98@gmail.com>
2021-12-09 15:23:02 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,38 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+var name:string = "John"; 
+var score1:number = 50;
+var score2:number = 42.50
+var sum = score1 + score2 
+console.log("name"+name) 
+console.log("first score: "+score1) 
+console.log("second score: "+score2) 
+console.log("sum of the scores: "+sum)
+
+//Type Assertion
+var str = '1' 
+var str2:number = <number> <any> str   //str is now of type number 
+console.log(typeof(str2))
+
+//Inferred Typing
+var num = 2;    // data type inferred as  number 
+console.log("value of num "+num); 
+num = "12";
+console.log(num);
+
+//Variable Scope
+var global_num = 12          //global variable 
+class Numbers { 
+   num_val = 13;             //class variable 
+   static sval = 10;         //static field 
+   
+   storeNum():void { 
+      var local_num = 14;    //local variable 
+   } 
+} 
+console.log("Global num: "+global_num)  
+console.log(Numbers.sval)   //static variable  
+var obj = new Numbers(); 
+console.log("Global num: "+obj.num_val) 

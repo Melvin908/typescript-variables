522cc833bc966ac5bdc649d53fbf497157ac82b3
Melvin908 <melvincyriac98@gmail.com>
2021-12-10 15:28:24 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -86,3 +86,29 @@ for(num=0;num<=20;num++) {
    count++
 }
 console.log (" The count of odd values between 0 and 20 is: "+count) 
+
+
+//FUNCTIONS
+//Optional Parameters
+function disp_details(id:number,name:string,mail_id?:string) { 
+   console.log("ID:", id); 
+   console.log("Name",name); 
+   
+   if(mail_id!=undefined)  
+   console.log("Email Id",mail_id); 
+}
+disp_details(123,"John");
+disp_details(111,"mary","mary@xyz.com");
+
+//Rest Parameters
+function addNumbers(...nums:number[]) {  
+   var i;   
+   var sum:number = 0; 
+   
+   for(i = 0;i<nums.length;i++) { 
+      sum = sum + nums[i]; 
+   } 
+   console.log("sum of the numbers",sum) 
+} 
+addNumbers(1,2,3) 
+addNumbers(10,10,10,10,10)

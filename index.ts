8ffa2d2f7fef1996097191634f5e75fe66392f76
Melvin908 <melvincyriac98@gmail.com>
2021-12-10 14:21:35 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -55,3 +55,34 @@ console.log(msg)
 var num:number = -2 
 var result = num > 0 ?"positive":"non-positive" 
 console.log(result)
+
+//hile versus do..while
+var n:number = 5 
+while(n > 5) { 
+   console.log("Entered while") 
+} 
+do { 
+   console.log("Entered do…while") 
+} 
+while(n>5)
+
+var i:number = 1 
+while(i<=10) { 
+   if (i % 5 == 0) {   
+      console.log ("The first multiple of 5  between 1 and 10 is : "+i) 
+      break     //exit the loop if the first multiple is found 
+   } 
+   i++ 
+}
+
+//continue Statement
+var num:number = 0
+var count:number = 0;
+
+for(num=0;num<=20;num++) {
+   if (num % 2==0) {
+      continue
+   }
+   count++
+}
+console.log (" The count of odd values between 0 and 20 is: "+count) 
